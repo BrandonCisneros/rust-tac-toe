97fa28a3022c78f9bf89524c41e47fecee5979b1
Brandon Cisneros <brcisneros1991@gmail.com>
2025-08-25 16:53:24 -0400

    still working

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::*;
+use std::vec;
+use std::io;
 
-pub struct Player {
-    id: u8,
-    name: Option<String>,
-    marker: Option<String>,
+struct Player {
+    name: String,
+    marker: String,
 }
 
 /*pub struct PlayerState {
@@ -13,14 +13,14 @@ pub struct Player {
 
 
 
-fn marker_validate(marker: Option<String>) -> bool {
-    let mut isValid: bool = true;
+fn marker_validate(marker: &String) -> bool {
+    let is_valid: bool = true;
 
-    if marker != Some("X") || "x" || "O" || "o" {
-        return !isValid;
+    if marker != "X" || marker != "x" || marker != "O" || marker != "o"{
+        return !is_valid;
     }
 
-    return isValid;
+    return is_valid;
 }
 
 
@@ -28,15 +28,13 @@ fn marker_validate(marker: Option<String>) -> bool {
 fn main() {
 
     let mut player_1 = Player {
-       id: 1,
-       marker: None,
-       name: None,
+       name: String::from(""),
+       marker: String::from(""),
     };
 
     let mut player_2 = Player {
-       id: 2,
-       marker: None,
-       name: None,
+       marker: "".to_string(),
+       name: "".to_string(),
     };
 
     let grid: Vec<Vec<&str>> = vec![
@@ -51,45 +49,47 @@ fn main() {
         
     println!("Welcome to RUST-TAC-TOE!!!\n");
 
+    //--- Player 1 Information
+    print!("Please enter the name for Player 1: ");
+    let mut player_name1 = String::new();
+    io::stdin().read_line(&mut player_name1).unwrap();
+    player_1.name = player_name1;
+    println!("\nPlayer 1: {}\n", player_1.name);
+    
+    //--- Player 2 information
+    print!("Please enter the name for Player 2: ");
+    let mut player_name2 = String::new();
+    io::stdin().read_line(&mut player_name2).unwrap();
+    player_2.name = player_name2;
+    println!("\nPlayer 2: {}\n", player_2.name);
+
+    println!("Hello {} & {}! Ready to play?\n", player_1.name, player_2.name);
+    println!("\nNow we have to decide who wants X's and who wants O's...\n");
+
+    println!("\n{:?}, please type in X or O: ", player_1.name);
+    player_1.marker = String::new();
+
+    let is_valid: bool = marker_validate(&player_1.marker);
+
+    if is_valid {
+       println!("\n{:?} chose {:?} !\n", player_1.name, player_1.marker);
+    }
+
+
     while !winner {
         let mut x_coord: u8 = 0;
         let mut y_coord: u8 = 0;
+
+        x_coord = 1;
+        y_coord = 1;
+
+        print!("X: {} | Y: {}\n", x_coord,y_coord);
 
         for i in &grid{
             println!("{:?}", i);
         }
 
-        
-        if player_1.name == None || player_2.name == None {
-
-            //--- Request Player 1 information
-            println!("Please enter the name for Player 1: \n");
-            player_1.name = Some(String::new());
-            
-            //--- Request Player 2 information
-            println!("Please enter the name for Player 2: \n");
-            player_2.name = Some(String::new());
-
-            println!("Hello {:?} & {:?}! Ready to play?\n", player_1.name, player_2.name);
-            println!("\nNow we have to decide who wants X's and who wants O's...\n");
-
-            println!("\n{:?}, please type in X or O: ", Some(player_1.name));
-            player_1.marker = Some(String::new());
-
-            let mut is_valid: bool = marker_validate(player_1.marker);
-
-            if is_valid {
-                println!("\n{:?} chose {:?} !\n", Some(player_1.name), Some(player_1.marker));
-            }
-
-
-
-
-        }
-
-
-
-
+        winner = true;
         
     }
 
